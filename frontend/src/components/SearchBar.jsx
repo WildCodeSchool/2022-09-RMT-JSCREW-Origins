a0f3513c1a04237b6c5d9bd1c418229d864dc5be
o6ris <loic.tsiry@gmail.com>
2022-12-09 18:18:58 +0100

    ajout du filtre pour la fonction search

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 function SearchBar() {
-  const [displayCategory, setDisplayCategory] = useState(false);
+  const [displayCategories, setDisplayCategories] = useState(false);
+  const [searchCategory, setSearchCategory] = useState("");
   const [myCategories, setMyCategories] = useState([]);
   useEffect(() => {
     fetch("http://localhost:5000/categories")
@@ -9,34 +10,50 @@ function SearchBar() {
       .then((categories) => setMyCategories(categories))
       .catch((error) => console.error(error));
   }, []);
+  const handleDisplayCategories = (e) => {
+    setSearchCategory(e.target.value);
+    if (searchCategory.length >= 0) {
+      setDisplayCategories(true);
+    } else if (searchCategory.length === 0) {
+      setDisplayCategories(false);
+    }
+  };
   return (
     <form className="flex flex-col items-center w-full">
       <label className="border-solid border-primary border-2 rounded-md p-3 w-3/4 lg:w-7/12 relative">
         <input
+          onChange={handleDisplayCategories}
           className="focus:outline-none"
           type="text"
           placeholder="Search game"
+          value={searchCategory}
         />
         <button
-          onClick={() => setDisplayCategory(!displayCategory)}
-          className="bg-primary text-white text-base rounded-md p-2 absolute absolute right-1 bottom-1 hover:bg-secondary hover:text-primary"
+          onClick={() => setDisplayCategories(!displayCategories)}
+          className={`bg-primary text-white text-base rounded-md p-2 absolute absolute right-1 bottom-1 hover:bg-secondary hover:text-primary ${
+            displayCategories && "focus:bg-secondary focus:text-primary"
+          }`}
           type="button"
         >
-          See all games
+          {!displayCategories ? "Show games" : "Hide games"}
         </button>
       </label>
-      {displayCategory && (
+      {displayCategories && (
         <div className="bg-primary w-3/4 lg:w-7/12 rounded-md">
           <ul className="flex flex-col">
-            {myCategories.map((myCategory) => (
-              <button
-                type="button"
-                key={myCategory.id}
-                className="text-white text-base self-start py-3 pl-5 hover:text-secondary hover:bg-white hover:bg-opacity-5 w-full flex"
-              >
-                {myCategory.Name}
-              </button>
-            ))}
+            {myCategories
+              .filter((myCategory) =>
+                myCategory.Name.startsWith(searchCategory)
+              )
+              .map((myCategory) => (
+                <button
+                  type="button"
+                  key={myCategory.id}
+                  className="text-white text-base self-start py-3 pl-5 hover:text-secondary hover:bg-white hover:bg-opacity-5 w-full flex"
+                >
+                  {myCategory.Name}
+                </button>
+              ))}
           </ul>
         </div>
       )}
