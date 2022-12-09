ec9955e45f7f1d4b0c88d6f0d9b15bfde678328e
charliepian <charlie.piancatelli@gmail.com>
2022-12-09 18:37:43 +0100

    Mofidication de la couleur du bouton Update

--- frontend/src/components/ButtonUpdate.jsx
+++ frontend/src/components/ButtonUpdate.jsx
@@ -4,7 +4,7 @@ function ButtonUpdate() {
   return (
     <button
       type="button"
-      className="w-32 border-solid border-primary text-primary hover:font-font-medium border-2 rounded-md bg-transparent hover:bg-secondary hover:text-primary py-2 px-4 border-primary hover:border-transparent"
+      className="w-32 border-solid border-secondary text-secondary hover:font-font-medium border-2 rounded-md bg-transparent hover:bg-secondary hover:text-primary py-2 px-4 border-primary hover:border-transparent"
     >
       UPDATE
     </button>
