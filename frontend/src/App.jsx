b42ed6cd6df1c48e1f9d19caa5bde6d2af4db036
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-12-09 09:10:06 +0100

    add dashboard and nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Dashboard from "./pages/Layout/Dashboard";
 import Category from "./pages/Back-office/Category";
 import Setting from "./pages/Back-office/Setting";
 import Slider from "./pages/Back-office/Slider";
 import Video from "./pages/Back-office/Video";
-import Nav from "./components/Nav";
 
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
-      <div>
+      <div className="flex">
         <Nav />
-        <Routes>
+        <Routes path="/Dashboard" element={<Dashboard />}>
           <Route path="/Category" element={<Category />} />
           <Route path="/Setting" element={<Setting />} />
           <Route path="/Slider" element={<Slider />} />
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,21 +2,45 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/Category">Categorie</Link>
-        </li>
-        <li>
-          <Link to="/Setting">Setting</Link>
-        </li>
-        <li>
-          <Link to="/Slider">Slider</Link>
-        </li>
-        <li>
-          <Link to="/Video">Video</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <div className="flexw-1/3 h-screen bg-gray-900 flex-col">
+        <div className="flex flex-col justify-start items-center   px-6 border-b border-gray-600 w-full  ">
+          <div>
+            <Link to="/Setting">
+              <button
+                className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+                type="button"
+              >
+                <p className="text-base leading-4  ">Setting</p>
+              </button>
+            </Link>
+            <Link to="/Slider">
+              <button
+                className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+                type="button"
+              >
+                <p className="text-base leading-4  ">Slider</p>
+              </button>
+            </Link>
+            <Link to="/Video">
+              <button
+                className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2 w-full md:w-52"
+                type="button"
+              >
+                <p className="text-base leading-4  ">Video</p>
+              </button>
+            </Link>
+            <Link to="/Category">
+              <button
+                className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+                type="button"
+              >
+                <p className="text-base leading-4  ">Category</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
