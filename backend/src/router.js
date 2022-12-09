def3957ca2b7c36e1b32c889a9932095287f997b
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-12-09 16:15:01 +0100

    hot fixe 2

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const categoryControllers = require("./controllers/categoryControllers");
-const VideoControllers = require("./controllers/videoControllers");
+const videoControllers = require("./controllers/videoControllers");
 const seetingControllers = require("./controllers/seetingControllers");
 
 // ----------- EXEMPLE DES ROUTES ------------
@@ -20,11 +20,11 @@ router.put("/categories/:id", categoryControllers.edit);
 router.post("/categories", categoryControllers.add);
 router.delete("/categories/:id", categoryControllers.destroy);
 
-router.get("/videos", VideoControllers.browse);
-router.get("/videos/:id", VideoControllers.read);
-router.put("/videos/:id", VideoControllers.edit);
-router.post("/videos", VideoControllers.add);
-router.delete("/videos/:id", VideoControllers.destroy);
+router.get("/videos", videoControllers.browse);
+router.get("/videos/:id", videoControllers.read);
+router.put("/videos/:id", videoControllers.edit);
+router.post("/videos", videoControllers.add);
+router.delete("/videos/:id", videoControllers.destroy);
 
 router.get("/user", seetingControllers.browse);
 router.get("/user/:id", seetingControllers.read);
