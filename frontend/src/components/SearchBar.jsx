2d0246380893fed7debdab97e90c408bfcb45664
o6ris <loic.tsiry@gmail.com>
2022-12-09 12:47:35 +0100

    fix du style de la nav + ajout de valeur de test

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
+const tests = ["tes1", "test2", "test3", "test4"];
+
 function SearchBar() {
   return (
     <form className="flex flex-col items-center w-full">
-      <input
-        className="border-solid border-primary border-2 rounded-md p-2 w-3/4"
-        type="text"
-        placeholder="Choose game"
-      />
-      <div>
-        <ul>
-          <li>test</li>
+      <label className="border-solid border-primary border-2 rounded-md p-3 w-3/4 relative">
+        <input
+          className="focus:outline-none"
+          type="text"
+          placeholder="Search game"
+        />
+        <button
+          className="bg-primary text-white rounded-md p-1 absolute absolute right-1 bottom-0.5"
+          type="button"
+        >
+          Search
+        </button>
+      </label>
+      <div className="bg-primary w-3/4 rounded-md">
+        <ul className="flex flex-col">
+          {tests.map((test) => (
+            <li className="text-white py-3 pl-5 hover:text-secondary">
+              {test}
+            </li>
+          ))}
         </ul>
       </div>
     </form>
