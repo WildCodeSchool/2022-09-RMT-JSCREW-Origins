2310250c8a065b690ec9de1ff2da649249eb33dc
charliepian <charlie.piancatelli@gmail.com>
2022-12-09 17:46:26 +0100

    Mise en place du HTML du form de connexion

--- frontend/src/components/ConnectForm.jsx
+++ frontend/src/components/ConnectForm.jsx
@@ -2,28 +2,26 @@ import React from "react";
 
 function ConnectForm() {
   return (
-    <div>
-      <form>
-        <div className="mb-4">
-          <label htmlFor="username">Username</label>
-          <input
-            className="shadow appearance-none border w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
-            id="username"
-            type="text"
-            placeholder="Username"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            className="shadow appearance-none border w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-        </div>
-      </form>
-    </div>
+    <form className="max-w-xs w-full">
+      <div className="flex flex-col p-2">
+        <label htmlFor="username">Username</label>
+        <input
+          className="border-solid border-primary border-2 rounded-md p-3"
+          id="username"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+      <div className="flex flex-col p-2">
+        <label htmlFor="password">Password</label>
+        <input
+          className="border-solid border-primary border-2 rounded-md p-3"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+      </div>
+    </form>
   );
 }
 
