2894a665bed89de9effd203f33df490b6f823570
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-12-14 14:18:16 +0100

    add nav for slider

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [displaySlider, setDisplaySlider] = useState(false);
+
+  const handleDisplaymenu = () => {
+    setDisplaySlider(!displaySlider);
+  };
+
   return (
     <div className="bg-primary">
       <div className="flex flex-col h-screen px-6 py-5 border-b justify-between">
@@ -16,34 +23,39 @@ export default function Nav() {
               <button
                 className="flex justify-start items-center space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
                 type="button"
+                onClick={handleDisplaymenu}
               >
                 <p className="text-base leading-4">Sliders</p>
               </button>
-              <div className="hidden onclick:visible block">
-                <Link
-                  to="slider"
-                  className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
-                >
-                  <p className="text-base leading-4">Sliders 1</p>
-                </Link>
-                <Link
-                  to="slider"
-                  className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
-                >
-                  <p className="text-base leading-4">Sliders 2</p>
-                </Link>
-                <Link
-                  to="slider"
-                  className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
-                >
-                  <p className="text-base leading-4">Sliders 3</p>
-                </Link>
-                <Link
-                  to="slider"
-                  className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
-                >
-                  <p className="text-base leading-4">Grille</p>
-                </Link>
+              <div className="block pl-5">
+                {displaySlider && (
+                  <>
+                    <Link
+                      to="slider"
+                      className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
+                    >
+                      <p className="text-base leading-4">Sliders 1</p>
+                    </Link>
+                    <Link
+                      to="slider"
+                      className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
+                    >
+                      <p className="text-base leading-4">Sliders 2</p>
+                    </Link>
+                    <Link
+                      to="slider"
+                      className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
+                    >
+                      <p className="text-base leading-4">Sliders 3</p>
+                    </Link>
+                    <Link
+                      to="slider"
+                      className="flex place-self-end space-x-6 hover:text-secondary focus:text-secondary text-white rounded px-3 py-2 w-full"
+                    >
+                      <p className="text-base leading-4">Grille</p>
+                    </Link>
+                  </>
+                )}
               </div>
             </div>
             <Link
