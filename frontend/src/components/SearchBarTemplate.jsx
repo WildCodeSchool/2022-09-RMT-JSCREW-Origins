584d699fad110843267f0f9424cd72b19d4dd6b0
o6ris <loic.tsiry@gmail.com>
2022-12-17 10:55:41 +0100

    z-index 1 sur le menu déroulant pour qu'il passe en 1er plan

--- frontend/src/components/SearchBarTemplate.jsx
+++ frontend/src/components/SearchBarTemplate.jsx
@@ -71,7 +71,7 @@ function SearchBar({
         </button>
       </label>
       {(displayData || searchData.length > 0) && (
-        <div className="bg-primary w-3/4 lg:w-7/12 rounded-md absolute top-12 mt-1">
+        <div className="bg-primary w-3/4 lg:w-7/12 rounded-md absolute z-10 top-12 mt-1">
           <ul className="flex flex-col">
             {data
               .filter((myData) =>
