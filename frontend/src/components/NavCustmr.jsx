56da63b30ce5b91546b7bf4e65890d68307c1b6b
o6ris <loic.tsiry@gmail.com>
2022-12-18 19:49:36 +0100

    ajout du menu pour la version mobile + disparition de la nav quand on est sur le dashboard

--- frontend/src/components/NavCustmr.jsx
+++ frontend/src/components/NavCustmr.jsx
@@ -1,29 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function NavCustmr() {
+  const [isMenuDisplayed, setIsMenuDisplayed] = useState(false);
+  const { pathname } = useLocation();
+
   return (
-    <nav className="fixed text-white p-7 w-full">
-      {/* Version Mobile */}
-      <ul className="md:hidden flex justify-between w-full">
-        <Link to="Home">Home</Link>
-        <button type="button">Burger</button>
-      </ul>
-      <div className="hidden flex justify-between">
-        <Link to="All-videos">All videos</Link>
-        <Link to="Dashboard">Back office</Link>
-        <Link to="Login">Login</Link>
-      </div>
-      {/* Version Desktop */}
-      <ul className="hidden md:grid grid-cols-2 gap-4 w-full">
-        <Link to="Home">Home</Link>
-        <div className="flex justify-between">
-          <Link to="All-videos">All videos</Link>
-          <Link to="Dashboard">Back office</Link>
-          <Link to="Login">Login</Link>
-        </div>
-      </ul>
-    </nav>
+    // Si on est dirigé vers le dashboard, la nav disparraît
+    pathname !== "/Origins/Dashboard" && (
+      <nav className="fixed text-white p-7 w-full">
+        {/* Version Mobile */}
+        <ul className="md:hidden flex justify-between w-full">
+          <Link to="Home">Home</Link>
+          <button
+            onClick={() => setIsMenuDisplayed(!isMenuDisplayed)}
+            type="button"
+          >
+            Burger
+          </button>
+        </ul>
+        {isMenuDisplayed && (
+          <div className="flex flex-col w-full items-center mt-5 gap-3">
+            <Link
+              onClick={() => setIsMenuDisplayed(false)}
+              className="hover:text-secondary hover:font-bold"
+              to="All-videos"
+            >
+              All videos
+            </Link>
+            <Link
+              onClick={() => setIsMenuDisplayed(false)}
+              className="hover:text-secondary hover:font-bold"
+              to="Dashboard"
+            >
+              Dashboard
+            </Link>
+            <Link
+              onClick={() => setIsMenuDisplayed(false)}
+              className="hover:text-secondary hover:font-bold"
+              to="Login"
+            >
+              Login
+            </Link>
+          </div>
+        )}
+        {/* Version Desktop */}
+        <ul className="hidden md:grid grid-cols-2 gap-4 w-full">
+          <Link to="Home">Home</Link>
+          <div className="flex justify-between">
+            <Link to="All-videos">All videos</Link>
+            <Link to="Dashboard">Back office</Link>
+            <Link to="Login">Login</Link>
+          </div>
+        </ul>
+      </nav>
+    )
   );
 }
 
