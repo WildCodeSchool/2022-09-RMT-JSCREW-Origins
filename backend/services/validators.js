6a0e1ac0890990f72020e89be5eea68d01c84ffe
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-12-19 15:35:44 +0100

    ajout de la verif joi sur les videos

--- backend/services/validators.js
+++ backend/services/validators.js
@@ -6,6 +6,14 @@ const categorySchema = Joi.object({
   Description: Joi.string().max(1000).required(),
 });
 
+const videoSchema = Joi.object({
+  Name: Joi.string().min(2).max(500).required(),
+  id_Category: Joi.number().min(1).max(10).required(),
+  Url: Joi.string().max(1000).required(),
+  Description: Joi.string().max(1000).required(),
+  Premium: Joi.number().min(0).required(),
+});
+
 const validateCategory = (req, res, next) => {
   const { error } = categorySchema.validate(req.body, { abortEarly: false });
 
@@ -16,6 +24,17 @@ const validateCategory = (req, res, next) => {
   }
 };
 
+const validateVideo = (req, res, next) => {
+  const { error } = videoSchema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   validateCategory,
+  validateVideo,
 };
