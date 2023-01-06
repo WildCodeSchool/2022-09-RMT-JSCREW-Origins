a2134a026a2be14b6db8df69c575aec4c055ac3f
jordanvax <jordan.vaxelaire@hotmail.fr>
2023-01-06 12:14:14 +0100

    mep page ceo dashboardcategory

--- frontend/src/pages/Back-office/Category.jsx
+++ frontend/src/pages/Back-office/Category.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { Helmet } from "react-helmet";
 
 import apiConnection from "@services/apiConnection";
 import validateCategory from "@services/categoryValidators";
@@ -126,6 +127,14 @@ function Category() {
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Origin's Dashboard - Category</title>
+        <meta
+          name="description"
+          content="Manage the categories on your website from this page of your back office dashboard. Add, edit, or delete categories, and assign videos to each category."
+        />
+      </Helmet>
       <ToastContainer
         position="top-right"
         autoClose={4000}
