cd182e68d7e34656b51982e35649eddc27d4d958
o6ris <loic.tsiry@gmail.com>
2023-01-08 10:11:26 +0100

    import du provider dans app.jsx et wrap de toutes les routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Login from "@pages/CustmrSite/Login";
 import AllVideos from "@pages/CustmrSite/AllVideos";
 import OneVideo from "@pages/CustmrSite/OneVideo";
 
+import User from "./contexts/UserContext";
+
 import Dashboard from "./pages/Layout/Dashboard";
 import Category from "./pages/Back-office/Category";
 import Setting from "./pages/Back-office/Setting";
@@ -20,27 +22,29 @@ import "./App.css";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<CustmrSite />}>
-            <Route path="" element={<Home />} />
-            <Route path="All-videos" element={<AllVideos />} />
-            <Route path="OneVideo" element={<OneVideo />} />
-            <Route path="Dashboard" element={<Dashboard />} />
-            <Route path="Login" element={<Login />} />
-          </Route>
-          <Route path="/Dashboard" element={<Dashboard />}>
-            <Route path="Category" element={<Category />} />
-            <Route path="Setting/:id" element={<Setting />} />
-            <Route path="Slider_1" element={<Slider1 />} />
-            <Route path="Slider_2" element={<Slider2 />} />
-            <Route path="Slider_3" element={<Slider3 />} />
-            <Route path="Grille" element={<Grille />} />
-            <Route path="Video" element={<Video />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <User.UserProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<CustmrSite />}>
+              <Route path="" element={<Home />} />
+              <Route path="All-videos" element={<AllVideos />} />
+              <Route path="OneVideo" element={<OneVideo />} />
+              <Route path="Dashboard" element={<Dashboard />} />
+              <Route path="Login" element={<Login />} />
+            </Route>
+            <Route path="/Dashboard" element={<Dashboard />}>
+              <Route path="Category" element={<Category />} />
+              <Route path="Setting/:id" element={<Setting />} />
+              <Route path="Slider_1" element={<Slider1 />} />
+              <Route path="Slider_2" element={<Slider2 />} />
+              <Route path="Slider_3" element={<Slider3 />} />
+              <Route path="Grille" element={<Grille />} />
+              <Route path="Video" element={<Video />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </User.UserProvider>
   );
 }
