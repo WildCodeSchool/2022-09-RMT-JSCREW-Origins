3bbe21d58106bf530331c88b2d55cd9b4a228d3e
jordanvax <jordan.vaxelaire@hotmail.fr>
2023-01-08 21:33:16 +0100

    ajout du logo et d'helmet sur la page one video

--- frontend/src/pages/CustmrSite/OneVideo.jsx
+++ frontend/src/pages/CustmrSite/OneVideo.jsx
@@ -1,12 +1,14 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import apiConnection from "@services/apiConnection";
 
 function OneVideo() {
   const [video, setVideo] = useState();
 
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/videos/8`)
+    apiConnection
+      .get(`/videos/8`)
       .then((oneVideo) => {
         setVideo(oneVideo.data);
       })
@@ -14,27 +16,38 @@ function OneVideo() {
   }, []);
 
   return (
-    <div className="h-screen bg-primary">
-      <div className="pt-20 text-white h-full">
-        {video && (
-          <div className="md:flex h-full">
-            <iframe
-              className="w-full h-2/4 md:w-3/5 md:h-4/6 md:pl-10"
-              title={video.Name}
-              src={video.Url}
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-            <div className="p-10">
-              <h1 className="text-3xl md:mb-5">{video.Name}</h1>
-              <h2 className="text-xl md:mb-5">{video.Category}</h2>
-              <p>{video.Description}</p>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{video?.Name}</title>
+        <meta
+          name="description"
+          content="Configure the settings for your website from this page of your back office dashboard. Edit your account information, set up security, and customize the appearance of your site."
+        />
+        <link rel="icon" type="image/png" href="../src/assets/logo.png" />
+      </Helmet>
+      <div className="h-screen bg-primary">
+        <div className="pt-20 text-white h-full">
+          {video && (
+            <div className="md:flex h-full">
+              <iframe
+                className="w-full h-2/4 md:w-3/5 md:h-4/6 md:pl-10"
+                title={video.Name}
+                src={video.Url}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+              <div className="p-10">
+                <h1 className="text-3xl md:mb-5">{video.Name}</h1>
+                <h2 className="text-xl md:mb-5">{video.Category}</h2>
+                <p>{video.Description}</p>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
