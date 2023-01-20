cf91d47435530a274b48c1269c2851f05eb0fdc4
jordanvax <jordan.vaxelaire@hotmail.fr>
2023-01-20 15:17:20 +0100

    ajout d'helmet sur la page d'acceuil

--- frontend/src/pages/CustmrSite/Home.jsx
+++ frontend/src/pages/CustmrSite/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import Header from "@components/Header";
 import ClientSlider1 from "@components/ClientSlider1";
 
 function Home() {
   return (
-    <div className="bg-primary">
-      <Header />
-      <ClientSlider1 />
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Origin's Home</title>
+        <meta
+          name="description"
+          content="home page of origin's website, you can find videos and pictures of Esport"
+        />
+        <link rel="icon" type="image/png" href="../src/assets/logo.png" />
+      </Helmet>
+      <div className="bg-primary">
+        <Header />
+        <ClientSlider1 />
+      </div>
+    </>
   );
 }
 
