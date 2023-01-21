c178c3b01efd310217f2181511901830acc9bcea
o6ris <loic.tsiry@gmail.com>
2023-01-21 13:20:30 +0100

    test de faire apparaitre 5 fois un composant dans all video

---
+++ frontend/src/components/ClientGrid.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ClientGrid() {
+  return <div>ClientGrid</div>;
+}
+
+export default ClientGrid;
--- frontend/src/pages/CustmrSite/AllVideos.jsx
+++ frontend/src/pages/CustmrSite/AllVideos.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+// import apiConnection from "@services/apiConnection";
+import ClientGrid from "@components/ClientGrid";
 
 function AllVideos() {
+  const data = [1, 2, 3, 4, 5];
+  // Ici créer un script pour avoir le nombre de slider dans la table en dynamique
   return (
     <div className="h-screen bg-primary">
       <h1>All videos</h1>
+      <div>
+        {data.map((item) => (
+          <ClientGrid key={item} data={item} />
+        ))}
+      </div>
     </div>
   );
 }
