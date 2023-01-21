6fd87a6342ede8227cf9f37f6a851fd598c0886c
o6ris <loic.tsiry@gmail.com>
2023-01-21 11:58:48 +0100

    optimisation de la requete pour affichage des video par categories dans le slider 3

--- backend/src/models/SliderCategoryManager.js
+++ backend/src/models/SliderCategoryManager.js
@@ -6,14 +6,16 @@ class SliderCategory extends AbstractManager {
   }
 
   findByCategory(id, limit) {
-    return this.connection.query(
-      `select ${this.table}.id, ${this.table}.Number, category.Name as category, video.Name as videoName, video.Url, video.premium from ${this.table}
-      inner join category on ${this.table}.id_category = category.id
-      inner join video on ${this.table}.id_category = video.id_Category
-      where ${this.table}.id = ?
-      limit ?`,
-      [id, limit]
-    );
+    let query = `select ${this.table}.id, ${this.table}.Number, ${this.table}.id_category, category.Name as category, video.Name as videoName, video.Url, video.premium from ${this.table}
+    inner join category on ${this.table}.id_category = category.id
+    inner join video on ${this.table}.id_category = video.id_Category
+    where ${this.table}.id = ?`;
+    const value = [id];
+    if (limit) {
+      query += " limit ?";
+      value.push(limit);
+    }
+    return this.connection.query(query, value);
   }
 
   insert(displayById) {
