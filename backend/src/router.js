35c635938256d9f44623c8c3d3e029b9ccc949c6
o6ris <loic.tsiry@gmail.com>
2023-01-21 12:40:07 +0100

    fix des requete en front pour adapter avec la nouvelle route get slider by cat

--- backend/src/router.js
+++ backend/src/router.js
@@ -61,7 +61,6 @@ router.post("/slider", sliderControllers.add);
 router.delete("/slider/:id", sliderControllers.destroyByIdVideo);
 
 router.get("/sliderCategory/:id", sliderCategoryControllers.browse);
-router.get("/sliderCategory/:id", sliderCategoryControllers.read);
 router.put("/sliderCategory/:id", sliderCategoryControllers.edit);
 router.post("/sliderCategory/:id", sliderCategoryControllers.add);
 
