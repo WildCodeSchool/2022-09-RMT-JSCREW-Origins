d1bb060123ea634b29ba15bf092de5b155ad052d
o6ris <loic.tsiry@gmail.com>
2023-01-23 19:41:40 +0100

    affichage des videos en favoris

--- frontend/src/pages/CustmrSite/Favorites.jsx
+++ frontend/src/pages/CustmrSite/Favorites.jsx
@@ -18,7 +18,18 @@ function Favorites() {
   return (
     <div className="bg-primary flex flex-col items-center gap-y-5 pt-20">
       <h1 className="my-5 text-white text-xl">All my favorites videos</h1>
-      <div>{myFavorites[0].videoName}</div>
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 place-items-center mb-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {myFavorites.map((myFavorite) => (
+          <iframe
+            className="w-11/12"
+            title={myFavorite.Name}
+            src={myFavorite.Url}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        ))}
+      </div>
     </div>
   );
 }
