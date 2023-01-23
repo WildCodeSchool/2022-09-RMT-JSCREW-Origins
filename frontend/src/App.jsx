f3796f0f48ad335ddcbeddb385a06276fc34d223
o6ris <loic.tsiry@gmail.com>
2023-01-23 19:05:49 +0100

    creation de la page favorites

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "@pages/CustmrSite/Home";
 import Login from "@pages/CustmrSite/Login";
 import AllVideos from "@pages/CustmrSite/AllVideos";
 import OneVideo from "@pages/CustmrSite/OneVideo";
+import Favorites from "@pages/CustmrSite/Favorites";
 
 import Dashboard from "./pages/Layout/Dashboard";
 import Category from "./pages/Back-office/Category";
@@ -29,6 +30,7 @@ export default function App() {
             <Route path="Videos/:id" element={<OneVideo />} />
             <Route path="Dashboard" element={<Dashboard />} />
             <Route path="Login" element={<Login />} />
+            <Route path="Favorites" element={<Favorites />} />
           </Route>
           <Route path="/Dashboard" element={<Dashboard />}>
             <Route path="Category" element={<Category />} />
---
+++ frontend/src/pages/CustmrSite/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Favorites() {
+  return (
+    <div className="bg-primary flex flex-col items-center gap-y-5 pt-20">
+      <h1 className="my-5 text-white text-xl">All my favorites videos</h1>
+      <div>mon contenu</div>
+    </div>
+  );
+}
+
+export default Favorites;
