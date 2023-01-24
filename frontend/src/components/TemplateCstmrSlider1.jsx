65df9be64cb33a56dd20180525634bff47c8e14a
jordanvax <jordan.vaxelaire@hotmail.fr>
2023-01-24 14:37:58 +0100

    ajout du slider par type de jeu

--- frontend/src/components/TemplateCstmrSlider1.jsx
+++ frontend/src/components/TemplateCstmrSlider1.jsx
@@ -32,7 +32,7 @@ function TemplateCstmrSlider1({ url }) {
       <div className="w-full">
         <div className="flex flex-col items-start gap-1 w-full h-full py-6 sm:py-6 px-2">
           <h1 className="text-white mb-2 ml-3">
-            Best of {sliders[0]?.category ? sliders[0].category : "titre"}
+            {sliders[0]?.category ? sliders[0].category : "titre"}
           </h1>
           {/* Carousel for desktop and large size devices */}
           <CarouselProvider
--- frontend/src/pages/CustmrSite/OneVideo.jsx
+++ frontend/src/pages/CustmrSite/OneVideo.jsx
@@ -7,6 +7,7 @@ import { SiLinkedin } from "react-icons/si";
 import { RiLock2Fill } from "react-icons/ri";
 
 import apiConnection from "@services/apiConnection";
+import TemplateCstmrSlider1 from "@components/TemplateCstmrSlider1";
 import ButtonTemplate from "../../components/ButtonTemplate";
 import User from "../../contexts/UserContext";
 
@@ -57,80 +58,87 @@ function OneVideo() {
       </Helmet>
       <div className="h-screen bg-primary">
         <div className="pt-20 text-white h-full">
-          {video && (
-            <div className="md:flex md:pl-10 h-full">
-              {user && (
-                <iframe
-                  className="w-full h-2/4 md:w-3/5 md:h-4/6 md:pl-10"
-                  title={video.Name}
-                  src={video.Url}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-              )}
-              {!user && (
-                <div className="md:w-3/5 md:h-4/6  flex flex-col items-center justifier-center text-center mt-5 bg-[#00162B] rounded-3xl mx-5">
-                  <RiLock2Fill className="text-7xl md:text-9xl mt-5 md:mt-20" />
-                  <p className="text-1xl mt-3 md:text-4xl md:mt-10">
-                    This video is only available in premium
-                  </p>
-                  <ButtonTemplate
-                    buttonType="button"
-                    buttonText="SUBSCRIBE"
-                    buttonStyle="cstm_buttonSecondary mt-4 md:mt-10 mb-6"
-                    methodOnClick={() => navigate("/Login")}
+          <div>
+            {video && (
+              <div className="md:flex md:pl-10 h-full">
+                {user && (
+                  <iframe
+                    className="w-full h-2/4 md:w-3/5 md:h-4/6 md:pl-10"
+                    title={video.Name}
+                    src={video.Url}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
                   />
-                </div>
-              )}
-              <div className="p-10 md:w-2/5">
-                <h1 className="text-3xl md:mb-5">{video.Name}</h1>
-                <h2 className="text-xl md:mb-5">{video.Category}</h2>
-                <p>{video.Description}</p>
-                <div className="flex items-center gap-3 mt-3">
-                  <a
-                    href={`https://www.facebook.com/sharer/sharer.php?u=http%3A//${
-                      import.meta.env.VITE_FRONTEND_URL
-                    }/videos/${id}`}
-                    target="_blank"
-                    rel="noreferrer"
-                    style={{
-                      color: "#1b74e4",
-                      fontSize: "20px",
-                    }}
-                  >
-                    <ImFacebook2 />
-                  </a>
-                  <a
-                    href={`https://twitter.com/intent/tweet?text=https%3A//${
-                      import.meta.env.VITE_FRONTEND_URL
-                    }/videos/8`}
-                    target="_blank"
-                    rel="noreferrer"
-                    style={{
-                      color: "rgb(54 155 240)",
-                      fontSize: "23px",
-                    }}
-                  >
-                    <FaTwitterSquare />
-                  </a>
-                  <a
-                    href={`https://www.linkedin.com/shareArticle?mini=true&url=https%3A//${
-                      import.meta.env.VITE_FRONTEND_URL
-                    }/videos/8`}
-                    target="_blank"
-                    rel="noreferrer"
-                    style={{
-                      color: "#2366c2",
-                      fontSize: "20px",
-                    }}
-                  >
-                    <SiLinkedin />
-                  </a>
+                )}
+                {!user && (
+                  <div className="md:w-3/5 md:h-4/6  flex flex-col items-center justifier-center text-center mt-5 bg-[#00162B] rounded-3xl mx-5">
+                    <RiLock2Fill className="text-7xl md:text-9xl mt-5 md:mt-20" />
+                    <p className="text-1xl mt-3 md:text-4xl md:mt-10">
+                      This video is only available in premium
+                    </p>
+                    <ButtonTemplate
+                      buttonType="button"
+                      buttonText="SUBSCRIBE"
+                      buttonStyle="cstm_buttonSecondary mt-4 md:mt-10 mb-6"
+                      methodOnClick={() => navigate("/Login")}
+                    />
+                  </div>
+                )}
+                <div className="p-10 md:w-2/5">
+                  <h1 className="text-3xl md:mb-5">{video.Name}</h1>
+                  <h2 className="text-xl md:mb-5">{video.Category}</h2>
+                  <p>{video.Description}</p>
+                  <div className="flex items-center gap-3 mt-3">
+                    <a
+                      href={`https://www.facebook.com/sharer/sharer.php?u=http%3A//${
+                        import.meta.env.VITE_FRONTEND_URL
+                      }/videos/${id}`}
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{
+                        color: "#1b74e4",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <ImFacebook2 />
+                    </a>
+                    <a
+                      href={`https://twitter.com/intent/tweet?text=https%3A//${
+                        import.meta.env.VITE_FRONTEND_URL
+                      }/videos/8`}
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{
+                        color: "rgb(54 155 240)",
+                        fontSize: "23px",
+                      }}
+                    >
+                      <FaTwitterSquare />
+                    </a>
+                    <a
+                      href={`https://www.linkedin.com/shareArticle?mini=true&url=https%3A//${
+                        import.meta.env.VITE_FRONTEND_URL
+                      }/videos/8`}
+                      target="_blank"
+                      rel="noreferrer"
+                      style={{
+                        color: "#2366c2",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <SiLinkedin />
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+            {video && (
+              <TemplateCstmrSlider1
+                url={`/sliderCategory/${video?.id_Category}`}
+              />
+            )}
+          </div>
         </div>
       </div>
     </>
