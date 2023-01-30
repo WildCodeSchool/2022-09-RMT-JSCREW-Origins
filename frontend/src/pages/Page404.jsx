dd88e7c044a52f582182ced4ebd4630e841403c7
o6ris <loic.tsiry@gmail.com>
2023-01-30 15:51:01 +0100

    ajout du bouton back home sur la page 404

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import ButtonTemplate from "@components/ButtonTemplate";
 // eslint-disable-next-line import/no-relative-packages
 import page404 from "../../../backend/public/page-404.jpg";
 
 function Page404() {
+  const navigate = useNavigate();
   return (
     // <div className={`h-screen bg-cover bg-[url(${page404})]`} />
-    <div className="h-screen">
+    <div className="h-screen relative">
+      <ButtonTemplate
+        buttonType="button"
+        buttonText="BACK HOME"
+        buttonStyle="cstm_buttonSecondary absolute bottom-20 left-0 right-0 mx-auto text-center py-3"
+        methodOnClick={() => navigate("/")}
+      />
       <img
         className="w-full h-full object-cover"
         src={page404}
