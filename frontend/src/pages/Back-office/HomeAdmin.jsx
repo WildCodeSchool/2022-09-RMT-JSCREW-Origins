ff76e8ea4cd97ab1b29e0af35254e9a6bf6442a6
charliepian <charlie.piancatelli@gmail.com>
2023-01-30 14:35:12 +0100

    Ajout du filtre des 10 derniere videos

--- frontend/src/pages/Back-office/HomeAdmin.jsx
+++ frontend/src/pages/Back-office/HomeAdmin.jsx
@@ -3,10 +3,24 @@ import React, { useState, useEffect } from "react";
 import apiConnection from "@services/apiConnection";
 
 function HomeAdmin() {
+  const [myVideo, setMyVideo] = useState([
+    {
+      id: null,
+      Name: "",
+      Premium: 0,
+    },
+  ]);
   const [totalRowsUsers, setTotalRowsUsers] = useState([]);
   const [totalRowsAdmin, setTotalRowsAdmin] = useState([]);
   const [totalRowsVideos, setTotalRowsVideos] = useState([]);
   const [totalRowsFavorites, setTotalRowsFavorites] = useState([]);
+
+  const getVideo = () => {
+    apiConnection
+      .get(`/videos`)
+      .then((users) => setMyVideo(users.data))
+      .catch((error) => console.error(error));
+  };
 
   const totalUsers = () => {
     apiConnection
@@ -41,6 +55,7 @@ function HomeAdmin() {
     totalAdmin();
     totalVideos();
     totalFavorites();
+    getVideo();
   }, []);
 
   return (
@@ -49,6 +64,54 @@ function HomeAdmin() {
       <h1>Total Admin : {totalRowsAdmin}</h1>
       <h1>Total Videos : {totalRowsVideos}</h1>
       <h1>Total Like : {totalRowsFavorites}</h1>
+      <div className="flex flex-col items-center w-full m-4 pt-10">
+        <div className="flex flex-col items-center lg:w-6/12">
+          {myVideo.length > 0 && (
+            <table className="min-w-full text-center">
+              <thead className="bg-primary">
+                <tr>
+                  <th
+                    scope="col"
+                    className="text-xl rounded-tl-lg font-medium text-white px-6 py-4"
+                  >
+                    ID
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-xl font-medium text-white px-6 py-4"
+                  >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-xl rounded-tr-lg font-medium text-white px-6 py-4"
+                  >
+                    Premium
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {myVideo
+                  .slice(-10)
+                  .reverse()
+                  .map((OneVideo) => (
+                    <tr key={OneVideo.id}>
+                      <td className="bg-gray-50 px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {OneVideo.id}
+                      </td>
+                      <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {OneVideo.Name}
+                      </td>
+                      <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        {OneVideo.Premium}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
