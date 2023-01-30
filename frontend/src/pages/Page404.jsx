17b7da4f160134e199616e11aa9e47aa2fe3a1de
o6ris <loic.tsiry@gmail.com>
2023-01-30 15:38:57 +0100

    creation du design de la page 404

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+// eslint-disable-next-line import/no-relative-packages
+import page404 from "../../../backend/public/page-404.jpg";
 
 function Page404() {
   return (
-    <div>
-      <h2>page 404</h2>
+    // <div className={`h-screen bg-cover bg-[url(${page404})]`} />
+    <div className="h-screen">
+      <img
+        className="w-full h-full object-cover"
+        src={page404}
+        alt="page-404"
+      />
     </div>
   );
 }
