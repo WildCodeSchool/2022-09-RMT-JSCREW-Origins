56f99ccf76907dd98912a5ff770abfa621792226
charliepian <charlie.piancatelli@gmail.com>
2023-01-30 16:09:30 +0100

    Ajout du css

--- frontend/src/pages/Back-office/HomeAdmin.jsx
+++ frontend/src/pages/Back-office/HomeAdmin.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 import apiConnection from "@services/apiConnection";
+import { FaUserFriends, FaUserEdit } from "react-icons/fa";
+import { BsFillCameraVideoFill } from "react-icons/bs";
+import { AiFillHeart } from "react-icons/ai";
 
 function HomeAdmin() {
   const [myVideo, setMyVideo] = useState([
@@ -59,58 +62,103 @@ function HomeAdmin() {
   }, []);
 
   return (
-    <div>
-      <h1>Total Users : {totalRowsUsers}</h1>
-      <h1>Total Admin : {totalRowsAdmin}</h1>
-      <h1>Total Videos : {totalRowsVideos}</h1>
-      <h1>Total Like : {totalRowsFavorites}</h1>
-      <div className="flex flex-col items-center w-full m-4 pt-10">
-        <div className="flex flex-col items-center lg:w-6/12">
-          {myVideo.length > 0 && (
-            <table className="min-w-full text-center">
-              <thead className="bg-primary">
-                <tr>
-                  <th
-                    scope="col"
-                    className="text-xl rounded-tl-lg font-medium text-white px-6 py-4"
-                  >
-                    ID
-                  </th>
-                  <th
-                    scope="col"
-                    className="text-xl font-medium text-white px-6 py-4"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="text-xl rounded-tr-lg font-medium text-white px-6 py-4"
-                  >
-                    Premium
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {myVideo
-                  .slice(-10)
-                  .reverse()
-                  .map((OneVideo) => (
-                    <tr key={OneVideo.id}>
-                      <td className="bg-gray-50 px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {OneVideo.id}
-                      </td>
-                      <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {OneVideo.Name}
-                      </td>
-                      <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {OneVideo.Premium}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          )}
+    <div className="flex flex-col w-full items-center">
+      <div className="flex pt-10 gap-y-7 justify-center w-full px-5">
+        <div className="bg-primary flex justify-center overflow-y-auto rounded-lg bg-white shadow-xl transition-all p-2 m-2 w-full">
+          <div className="grid justify-items-center text-white">
+            <FaUserFriends
+              style={{
+                color: "#fff",
+                fontSize: "30px",
+              }}
+            />
+            <h2>Users</h2>
+            <p className="text-3xl text-white m-2">{totalRowsUsers}</p>
+          </div>
         </div>
+        <div className="bg-primary flex justify-center overflow-y-auto rounded-lg bg-white shadow-xl transition-all p-2 m-2 w-full">
+          <div className="grid justify-items-center text-white">
+            <FaUserEdit
+              style={{
+                color: "#fff",
+                fontSize: "30px",
+              }}
+            />
+            <h2>Admin</h2>
+            <p className="text-3xl text-white m-2">{totalRowsAdmin}</p>
+          </div>
+        </div>
+        <div className="bg-primary flex justify-center overflow-y-auto rounded-lg bg-white shadow-xl transition-all p-2 m-2 w-full">
+          <div className="grid justify-items-center text-white">
+            <BsFillCameraVideoFill
+              style={{
+                color: "#fff",
+                fontSize: "30px",
+              }}
+            />
+            <h2>Videos</h2>
+            <p className="text-3xl text-white m-2">{totalRowsVideos}</p>
+          </div>
+        </div>
+        <div className="bg-primary flex justify-center overflow-y-auto rounded-lg bg-white shadow-xl transition-all p-2 m-2 w-full">
+          <div className="grid justify-items-center text-white">
+            <AiFillHeart
+              style={{
+                color: "#fff",
+                fontSize: "30px",
+              }}
+            />
+            <h2>Like</h2>
+            <p className="text-3xl text-white m-2">{totalRowsFavorites}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center p-4">
+        <h2 className="m-4 text-xl">The last 5 videos uploaded</h2>
+        {myVideo.length > 0 && (
+          <table className="min-w-full text-center">
+            <thead className="bg-primary">
+              <tr>
+                <th
+                  scope="col"
+                  className="text-xl rounded-tl-lg font-medium text-white px-6 py-4"
+                >
+                  ID
+                </th>
+                <th
+                  scope="col"
+                  className="text-xl font-medium text-white px-6 py-4"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="text-xl rounded-tr-lg font-medium text-white px-6 py-4"
+                >
+                  Premium
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {myVideo
+                .slice(-5)
+                .reverse()
+                .map((OneVideo) => (
+                  <tr key={OneVideo.id}>
+                    <td className="bg-gray-50 px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {OneVideo.id}
+                    </td>
+                    <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {OneVideo.Name}
+                    </td>
+                    <td className="bg-gray-50 text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {OneVideo.Premium}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
